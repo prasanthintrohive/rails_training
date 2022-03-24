4c4c2fb005ab07d360a7d0c020a6a4900093391d
Aravind Raj <aravindraj.murugaiyan@introhive.com>
2022-03-24 11:43:00 +0530

    devise added and nav bar linked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     protected
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:admin,:name])
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:admin,:full_name])
         end
 end
