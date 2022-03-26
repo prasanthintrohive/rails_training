e2c3bcc8f3e89ec960bb1b35f6136beaceed28d3
aravind-the-coder <101354264+aravind-the-coder@users.noreply.github.com>
2022-03-26 10:22:48 +0530

    fix edit profile problex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
     protected
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:admin,:full_name])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:admin,:full_name])
         end
 end
